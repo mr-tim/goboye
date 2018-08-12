601b24ab7d60c959bafed4b9606e6e30cd0fac85
Tim Moran <tim@t-moran.com>
2018-08-12 23:49:17 +0100

    Dedupe a bunch of opcode handlers

--- src/goboye/cpu/opcode_handlers.go
+++ src/goboye/cpu/opcode_handlers.go
@@ -8,57 +8,25 @@ func unimplementedHandler(op opcode, p *processor) {
 
 func nopHandler(op opcode, p *processor) {}
 
-func load16BitToBC(op opcode, p *processor) {
-	load16BitToRegPair(p, RegisterPairBC)
+func load16BitToRegPair(rp registerPair) opcodeHandler {
+	return func(op opcode, p *processor) {
+		doLoad16BitToRegPair(p, rp)
+	}
 }
 
-func load16BitToDE(op opcode, p *processor) {
-	load16BitToRegPair(p, RegisterPairDE)
-}
-
-func load16BitToHL(op opcode, p *processor) {
-	load16BitToRegPair(p, RegisterPairHL)
-}
-
-func load16BitToSP(op opcode, p *processor) {
-	load16BitToRegPair(p, RegisterPairSP)
-}
-
-func load16BitToRegPair(p *processor, pair registerPair) {
+func doLoad16BitToRegPair(p *processor, pair registerPair) {
 	value := p.memory.ReadU16(p.registers.pc)
 	p.registers.pc += 2
 	p.registers.setRegisterPair(pair, value)
 }
 
-func load8BitToA(op opcode, p *processor) {
-	load8BitToReg(p, RegisterA)
+func load8BitToReg(r register) opcodeHandler {
+	return func(op opcode, p *processor) {
+		doLoad8BitToReg(p, r)
+	}
 }
 
-func load8BitToB(op opcode, p *processor) {
-	load8BitToReg(p, RegisterB)
-}
-
-func load8BitToC(op opcode, p *processor) {
-	load8BitToReg(p, RegisterC)
-}
-
-func load8BitToD(op opcode, p *processor) {
-	load8BitToReg(p, RegisterD)
-}
-
-func load8BitToE(op opcode, p *processor) {
-	load8BitToReg(p, RegisterE)
-}
-
-func load8BitToH(op opcode, p *processor) {
-	load8BitToReg(p, RegisterH)
-}
-
-func load8BitToL(op opcode, p *processor) {
-	load8BitToReg(p, RegisterL)
-}
-
-func load8BitToReg(p *processor, reg register) {
+func doLoad8BitToReg(p *processor, reg register) {
 	value := p.memory.ReadByte(p.registers.pc)
 	p.registers.pc++
 	p.registers.setRegister(reg, value)
@@ -107,43 +75,23 @@ func saveAToHLAddrDec(op opcode, p *processor) {
 	p.registers.hl--
 }
 
-func incrementBC(op opcode, p *processor) {
-	incrementRegPair(p, RegisterPairBC)
+func incrementRegPair(pair registerPair) opcodeHandler {
+	return func(op opcode, p *processor) {
+		doIncrementRegPair(p, pair)
+	}
 }
 
-func decrementBC(op opcode, p *processor) {
-	decrementRegPair(p, RegisterPairBC)
+func decrementRegPair(pair registerPair) opcodeHandler {
+	return func(op opcode, p *processor) {
+		doDecrementRegPair(p, pair)
+	}
 }
 
-func incrementDE(op opcode, p *processor) {
-	incrementRegPair(p, RegisterPairDE)
-}
-
-func decrementDE(op opcode, p *processor) {
-	decrementRegPair(p, RegisterPairDE)
-}
-
-func incrementHL(op opcode, p *processor) {
-	incrementRegPair(p, RegisterPairHL)
-}
-
-func decrementHL(op opcode, p *processor) {
-	decrementRegPair(p, RegisterPairHL)
-}
-
-func incrementSP(op opcode, p *processor) {
-	incrementRegPair(p, RegisterPairSP)
-}
-
-func decrementSP(op opcode, p *processor) {
-	decrementRegPair(p, RegisterPairSP)
-}
-
-func incrementRegPair(p *processor, rp registerPair) {
+func doIncrementRegPair(p *processor, rp registerPair) {
 	p.registers.setRegisterPair(rp, p.registers.getRegisterPair(rp)+1)
 }
 
-func decrementRegPair(p *processor, rp registerPair) {
+func doDecrementRegPair(p *processor, rp registerPair) {
 	p.registers.setRegisterPair(rp, p.registers.getRegisterPair(rp)-1)
 }
 
@@ -179,63 +127,19 @@ func decrementHLAddr(op opcode, p *processor) {
 	p.registers.setRegister(RegisterF, flags)
 }
 
-func incrementA(op opcode, p *processor) {
-	incrementRegister(p, RegisterA)
+func incrementReg(reg register) opcodeHandler {
+	return func(op opcode, p *processor) {
+		doIncrementRegister(p, reg)
+	}
 }
 
-func decrementA(op opcode, p *processor) {
-	decrementRegister(p, RegisterA)
+func decrementReg(reg register) opcodeHandler {
+	return func(op opcode, p *processor) {
+		doDecrementRegister(p, reg)
+	}
 }
 
-func incrementB(op opcode, p *processor) {
-	incrementRegister(p, RegisterB)
-}
-
-func decrementB(op opcode, p *processor) {
-	decrementRegister(p, RegisterB)
-}
-
-func incrementC(op opcode, p *processor) {
-	incrementRegister(p, RegisterC)
-}
-
-func decrementC(op opcode, p *processor) {
-	decrementRegister(p, RegisterC)
-}
-
-func incrementD(op opcode, p *processor) {
-	incrementRegister(p, RegisterD)
-}
-
-func decrementD(op opcode, p *processor) {
-	decrementRegister(p, RegisterD)
-}
-
-func incrementE(op opcode, p *processor) {
-	incrementRegister(p, RegisterE)
-}
-
-func decrementE(op opcode, p *processor) {
-	decrementRegister(p, RegisterE)
-}
-
-func incrementH(op opcode, p *processor) {
-	incrementRegister(p, RegisterH)
-}
-
-func decrementH(op opcode, p *processor) {
-	decrementRegister(p, RegisterH)
-}
-
-func incrementL(op opcode, p *processor) {
-	incrementRegister(p, RegisterL)
-}
-
-func decrementL(op opcode, p *processor) {
-	decrementRegister(p, RegisterL)
-}
-
-func incrementRegister(p *processor, reg register) {
+func doIncrementRegister(p *processor, reg register) {
 	oldValue := p.registers.getRegister(reg)
 	newValue := oldValue + 1
 	p.registers.setRegister(reg, newValue)
@@ -251,7 +155,7 @@ func incrementRegister(p *processor, reg register) {
 	p.registers.setRegister(RegisterF, flags)
 }
 
-func decrementRegister(p *processor, reg register) {
+func doDecrementRegister(p *processor, reg register) {
 	oldValue := p.registers.getRegister(reg)
 	newValue := oldValue - 1
 	p.registers.setRegister(reg, newValue)
