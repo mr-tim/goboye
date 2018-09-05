3141140d01486799e5a0d5aa66ba3f69d6c73027
Tim Moran <tim@t-moran.com>
2018-09-05 09:18:12 +0100

    Implement remaining main opcodes (other than DAA)

--- src/goboye/cpu/processor.go
+++ src/goboye/cpu/processor.go
@@ -8,7 +8,18 @@ type processor struct {
 }
 
 func (p *processor) readNextInstruction() opcode {
-	opCodeByte := p.memory.ReadByte(p.registers.pc)
-	p.registers.pc++
+	opCodeByte := p.Read8BitImmediate()
 	return lookupOpcode(opCodeByte)
+}
+
+func (p *processor) Read8BitImmediate() byte {
+	value := p.memory.ReadByte(p.registers.pc)
+	p.registers.pc++
+	return value
+}
+
+func (p *processor) Read16BitImmediate() uint16 {
+	value := p.memory.ReadU16(p.registers.pc)
+	p.registers.pc += 2
+	return value
 }
