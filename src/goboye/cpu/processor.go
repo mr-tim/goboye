d187ae3aac969c7dc0b8baed4bc16fccd3ff3811
Tim Moran <tim@t-moran.com>
2018-09-09 22:01:50 +0100

    Basic debugger for cpu operations

--- src/goboye/cpu/processor.go
+++ src/goboye/cpu/processor.go
@@ -1,6 +1,17 @@
 package cpu
 
-import "goboye/memory"
+import (
+	"goboye/memory"
+	"fmt"
+)
+
+type Processor interface {
+	NextInstruction() Opcode
+	DoNextInstruction()
+	DebugRegisters() string
+	GetRegister(reg register) uint8
+	GetRegisterPair(pair registerPair) uint16
+}
 
 type processor struct {
 	registers *registers
@@ -10,6 +21,12 @@ type processor struct {
 func (p *processor) readNextInstruction() opcode {
 	opCodeByte := p.Read8BitImmediate()
 	return lookupOpcode(opCodeByte)
+}
+
+func (p *processor) NextInstruction() Opcode {
+	b := p.memory.ReadByte(p.registers.pc)
+	o := lookupOpcode(b)
+	return &o
 }
 
 func (p *processor) Read8BitImmediate() byte {
@@ -22,4 +39,31 @@ func (p *processor) Read16BitImmediate() uint16 {
 	value := p.memory.ReadU16(p.registers.pc)
 	p.registers.pc += 2
 	return value
+}
+
+func (p *processor) DoNextInstruction() {
+	o := p.readNextInstruction()
+	o.handler(o, p)
+}
+
+func (p *processor) DebugRegisters() string {
+	return fmt.Sprintf("{af:%04x bc:%04x de:%04x hl:%04x sp:%04x}",
+		p.registers.af, p.registers.bc, p.registers.de, p.registers.hl, p.registers.sp)
+}
+
+func (p *processor) GetRegister(reg register) uint8 {
+	return p.registers.getRegister(reg)
+}
+
+func (p *processor) GetRegisterPair(regPair registerPair) uint16 {
+	return p.registers.getRegisterPair(regPair)
+}
+
+func NewProcessor(memory memory.MemoryMap) Processor {
+	p := processor{
+		registers: &registers{},
+		memory: memory,
+	}
+	p.registers.pc = uint16(0x0000)
+	return &p
 }
---
+++ src/goboye/debugger/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"goboye/memory"
+	"goboye/cpu"
+	"os"
+)
+
+func main() {
+	fmt.Println("Running...")
+
+	buf := make([]byte, memory.MEM_SIZE)
+	mm := memory.NewMemoryMapWithBytes(buf)
+	e := mm.LoadRomImage(os.Args[1])
+	if e != nil {
+		panic(e)
+	}
+
+	p := cpu.NewProcessor(mm)
+	for i:=0; i<20000; i++ {
+		o := p.NextInstruction()
+		pc := p.GetRegisterPair(cpu.RegisterPairPC)
+		argWidth := o.PayloadLength()
+		if argWidth == 1 {
+			arg := mm.ReadByte(pc+1)
+			fmt.Printf("0x%04x: %s %s 0x%02x ", pc, p.DebugRegisters(), o.Disassembly(), arg)
+		} else if argWidth == 2 {
+			arg := mm.ReadU16(pc+1)
+			fmt.Printf("0x%04x: %s %s 0x%04x ", pc, p.DebugRegisters(), o.Disassembly(), arg)
+		} else {
+			fmt.Printf("0x%04x: %s %s ", pc, p.DebugRegisters(), o.Disassembly())
+		}
+		fmt.Println()
+		p.DoNextInstruction()
+	}
+}
