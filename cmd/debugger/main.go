3da70fcd917f14eace0b3a84561bbb83d6d69854
Tim Moran <tim@t-moran.com>
2020-02-08 16:46:12 +0000

    Get basic debugger screen working

--- cmd/debugger/main.go
+++ cmd/debugger/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/jroimartin/gocui"
 
@@ -13,6 +14,16 @@ import (
 )
 
 func main() {
+	f, err := os.OpenFile("debugger.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
+	log.Printf(strings.Repeat("=", 80))
+	log.Printf("Starting debugger...")
+
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
 		log.Panicln(err)
@@ -27,6 +38,8 @@ func main() {
 		os.Exit(1)
 	}
 
+	// set up the emulator
+	log.Printf("Loading rom: %s", os.Args[1])
 	e := mm.LoadRomImage(os.Args[1])
 	if e != nil {
 		panic(e)
@@ -35,12 +48,20 @@ func main() {
 	p := cpu.NewProcessor(mm)
 	pc := p.GetRegisterPair(cpu.RegisterPairPC)
 
+	// set up ui
+	log.Printf("Creating ui...")
 	da := cpu.NewDisassembler(mm)
 	dw := widgets.NewDisassemblyWidget(da, pc)
 	rw := widgets.NewRegistersWidget()
 	g.SetManager(dw, rw)
 
+	err = g.SetKeybinding("", gocui.KeyEnter, gocui.ModNone, dw.Increment)
+	if err != nil {
+		log.Fatalf("Failed to set key binding for increment: %s", err)
+	}
+
 	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Printf("Received ctrl-c")
 		log.Panicln(err)
 	}
 
--- internal/pkg/debugger/widgets/disassembly_widget.go
+++ internal/pkg/debugger/widgets/disassembly_widget.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"github.com/jroimartin/gocui"
 	"github.com/mr-tim/goboye/internal/pkg/cpu"
+	"log"
 )
+
+const DisassemblyViewName = "disassembly"
 
 type DisassemblyWidget struct {
 	da        *cpu.Disassembler
@@ -12,15 +15,16 @@ type DisassemblyWidget struct {
 }
 
 func (d *DisassemblyWidget) Layout(g *gocui.Gui) error {
+	log.Printf("Running DisassemblyWidget.Layout")
 	_, maxY := g.Size()
-	v, err := g.SetView("disassembly", 0, 0, 30, maxY-1)
+	v, err := g.SetView(DisassemblyViewName, 0, 0, 30, maxY-1)
 	if err != nil && err != gocui.ErrUnknownView {
 		return err
 	}
 	v.Title = "Disassembly"
 	v.Clear()
 	// TODO: Handle scrolling in the disassembly view
-	d.da.SetPos(0)
+	d.da.SetPos(d.currentPc)
 	for i := 0; i < maxY-1; i++ {
 		addr, o, payload := d.da.GetNextInstruction()
 		pointer := " "
@@ -29,11 +33,20 @@ func (d *DisassemblyWidget) Layout(g *gocui.Gui) error {
 		}
 		fmt.Fprintf(v, " %s 0x%04x %s\n", pointer, addr, o.DisassemblyWithArg(payload))
 	}
+
 	return nil
 }
 
 func (d *DisassemblyWidget) SetPc(newPc uint16) {
 	d.currentPc = newPc
+}
+
+func (d *DisassemblyWidget) Increment(g *gocui.Gui, v *gocui.View) error {
+	log.Printf("Incrementing...")
+	pos, _, _ := d.da.GetNextInstruction()
+	d.SetPc(pos)
+	log.Printf("Pc = %#v", pos)
+	return nil
 }
 
 func NewDisassemblyWidget(disassembler *cpu.Disassembler, currentPc uint16) *DisassemblyWidget {
